1922c1f6fe5003551b25f757db5b8574ea056f08
ArmanKhanTech <ak2341776@gmail.com>
2024-11-12 18:02:05 +0530

    Update Home component: add ranking and position logic for leaderboard entries

--- src/Home.jsx
+++ src/Home.jsx
@@ -44,6 +44,26 @@ function Home() {
         if (data[data.length - 1]["User Name"] === "") {
           data.pop();
         }
+
+        let rank = 1;
+        let position = 0;
+        for (let i = 0; i < data.length; i++) {
+          position++;
+          data[i].position = position;
+          if (i === 0) {
+            data[i].rank = rank;
+          } else {
+            if (data[i].percentage === data[i - 1].percentage) {
+              data[i].rank = rank;
+            } else {
+              rank++;
+              data[i].rank = rank;
+            }
+          }
+          if (data[i].percentage === 0) {
+            rank++;
+          }
+        }
       },
     });
   }, []);
@@ -188,6 +208,9 @@ function Home() {
                   <thead className="bg-gray-50">
                     <tr>
                       <th scope="col" className="w-1/8 px-4 py-3.5 font-semibold text-left rtl:text-right text-gray-500">
+                        Sr No.
+                      </th>
+                      <th scope="col" className="w-1/8 px-4 py-3.5 font-semibold text-left rtl:text-right text-gray-500">
                         Rank
                       </th>
                       <th scope="col" className="w-1/8 py-3.5 px-4 font-semibold text-left rtl:text-right text-gray-500">
@@ -220,6 +243,11 @@ function Home() {
                     ) : (
                       filteredData.map((row, index) => (
                         <tr key={index}>
+                          <td className="px-4 py-4 text-md whitespace-nowrap">
+                            <div>
+                              <h4 className="text-gray-700 pl-2">{row["position"]}</h4>
+                            </div>
+                          </td>
                           <td className="px-4 py-4 text-md font-bold whitespace-nowrap">
                             <div>
                               <h4 className="text-gray-700 pl-2">{row["rank"]}</h4>
